16b2b52f301359cdb412c6c89d306331ef8a1921
JeffNolasco <95046541+JeffNolasco@users.noreply.github.com>
2023-06-14 16:24:57 -0500

    Inicio
    Primer repositorio

---
+++ src/main/java/ec/edu/espe/arquitectura/facturacion/service/ClienteService.java
@@ -0,0 +1,77 @@
+package ec.edu.espe.arquitectura.facturacion.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import ec.edu.espe.arquitectura.facturacion.model.Cliente;
+import ec.edu.espe.arquitectura.facturacion.repository.ClienteRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class ClienteService {
+    
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    
+    public Optional<Cliente> obtainByCode(Integer code) {
+        return this.clienteRepository.findById(code);
+    }
+
+    public List<Cliente> listByTipoIdentificacionAndPattern(String tipoIdentificacion, String patron) {
+        if ("RUC".equals(tipoIdentificacion)) {
+            return this.clienteRepository.findByApellidoLikeOrderByApellido(patron);
+        } else {
+            return this.clienteRepository.findByRazonSocialLikeOrderByRazonSocial(patron);
+        }
+    }
+
+    @Transactional // en operaciones solo de lectura no necesito transacciones
+    public Cliente create(Cliente cliente) {
+        Cliente clienteTmp = this.clienteRepository.findByTipoIdentificacionAndIdentificacion(cliente.getTipoIdentificacion(), cliente.getIdentificacion());
+        if (clienteTmp == null) {
+            return this.clienteRepository.save(cliente);
+        } else {
+            throw new RuntimeException("Cliente ya existe"); //se usa RuntimeException debido al @Transactional
+        }
+    }
+
+    @Transactional
+    public Cliente update(Cliente cliente) {
+        Optional<Cliente> clienteOpt = this.clienteRepository.findById(cliente.getCodigo());
+        if (clienteOpt.isPresent()) { //si es que existe el cliente
+            Cliente clienteTmp = clienteOpt.get();  //memoria del entity manager
+            clienteTmp.setApellido(cliente.getApellido());
+            clienteTmp.setNombre(cliente.getNombre());
+            clienteTmp.setRazonSocial(cliente.getRazonSocial());
+            clienteTmp.setDireccion(cliente.getDireccion());
+            clienteTmp.setTelefono(cliente.getTelefono());
+            clienteTmp.setEmail(cliente.getEmail());
+            this.clienteRepository.save(clienteTmp); //update
+            return clienteTmp;
+        } else {
+            throw new RuntimeException("Cliente que desea modificar no esta registrado");
+        }
+    }
+
+    @Transactional
+    public void delete(Integer clienteCode) {
+        try {
+            Optional<Cliente> clienteOpt =  this.clienteRepository.findById(clienteCode);
+            if (clienteOpt.isPresent()) {
+                this.clienteRepository.delete(clienteOpt.get());
+            } else {
+                throw new RuntimeException("El cliente no esta registrado: "+clienteCode);
+            }
+        } catch (RuntimeException rte) {
+            throw new RuntimeException("No se puede eliminar el cliente con Codigo: "+ clienteCode, rte);
+        }
+    }
+
+
+}
